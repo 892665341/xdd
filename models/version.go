139d7a859f7831fbbd043e99f68ca89daeb18f83
892665341 <58127162+892665341@users.noreply.github.com>
2021-08-29 13:40:28 +0800

    Update version.go

--- models/version.go
+++ models/version.go
@@ -61,26 +61,5 @@ func initVersion() {
 
 func Update(msgs ...interface{}) error {
 	sendMessagee("小滴滴开始拉取代码", msgs...)
-	rtn, err := exec.Command("sh", "-c", "cd "+ExecPath+" && git stash && git pull").Output()
-	if err != nil {
-		return errors.New("小滴滴拉取代失败：" + err.Error())
-	}
-	t := string(rtn)
-	if !strings.Contains(t, "changed") {
-		if strings.Contains(t, "Already") || strings.Contains(t, "已经是最新") {
-			return errors.New("小滴滴已是最新版啦")
-		} else {
-			return errors.New("小滴滴拉取代失败：" + t)
-		}
-	} else {
-		sendMessagee("小滴滴拉取代码成功", msgs...)
-	}
-	sendMessagee("小滴滴正在编译程序", msgs...)
-	rtn, err = exec.Command("sh", "-c", "cd "+ExecPath+" && go build -o "+pname).Output()
-	if err != nil {
-		return errors.New("小滴滴编译失败：" + err.Error())
-	} else {
-		sendMessagee("小滴滴编译成功", msgs...)
-	}
-	return nil
+	return 1
 }
